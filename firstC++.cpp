6d8f2677aed8faaf2d5123871543b49efe1d7d5d
nohyunhwan <47073331+nohyunhwan@users.noreply.github.com>
2019-03-26 17:43:18 +0900

    Add files via upload

---
+++ firstC++.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+class Circle
+{
+	int radius;
+	int x, y;
+	double getArea;
+};
+class Triangle
+{
+	int a, b, c;
+	double height;
+	int center;
+	double getArea;
+};
+class Rectabngle
+{
+	int a, b, c, d;
+	double width;
+	double height;
+	double getArea;
+};
+
+int main(void)
+{
+
+}
